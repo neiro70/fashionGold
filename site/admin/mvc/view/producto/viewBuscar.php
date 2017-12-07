bcca03d6fe7f36046595b9f029fb74cd9dbfd65c
jlmartineza <jlmartineza@actinver.com.mx>
2017-12-06 23:38:30 -0600

    Cambio de php5.6

--- site/admin/mvc/view/producto/viewBuscar.php
+++ site/admin/mvc/view/producto/viewBuscar.php
@@ -15,11 +15,11 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
 
-    <meta charset="ISO-8859-1">
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link href="<?=$contexto?>/site/admin/css/bootstrap3-dialog/css/bootstrap-dialog.css" rel="stylesheet" type="text/css" />
