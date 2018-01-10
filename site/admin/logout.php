b53308d3db65e0f2df6b7438723f113a3695256f
Jorge Luis Martinez <neiro70@gmail.com>
2018-01-09 22:37:43 -0600

    Implementacion de administrador con encoding

--- site/admin/logout.php
+++ site/admin/logout.php
@@ -3,6 +3,14 @@
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
    session_destroy();
+   $context= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+   $var =explode("/",$context);
+   if (strpos($context, "localhost") !== false) {
+       $context="http://" .$var[0]."/".$var[1];
+   }else{
+       $context="http://" .$var[0];
+   }
+   
    
    echo 'Saliendo de la session del sistema';
    echo("<script>location.href = '".$context."/site/admin';</script>");
