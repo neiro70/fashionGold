9cef7d9c821112855d5c3086a0ef55779ad621eb
josorio <josorio@actinver.com.mx>
2018-01-11 09:33:33 -0600

    Se agrega template para modal responsivo.

--- site/collections/earrings.php
+++ site/collections/earrings.php
@@ -93,43 +93,7 @@
 		<?php include "../../site/template/header.php";?>
 		<?php include "../../site/template/content.php";?>
 		<?php include "../../site/template/footer.php";?>
-
-		<!-- Modal -->
-		<div class="container">
-			<div class="modal fade" id="myModalFrame">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header" style="background-color: #9D802E; color:#FFF;  border-top-left-radius: 5px; border-top-right-radius: 5px; ">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
-							<h4 class="modal-title" style="color:#FFF;">Detalle del Producto</h4>
-						</div>
-						<div class="modal-body">
-							<iframe src="" 
-									frameborder="0" 
-									id="targetiframe"
-									name="targetframe" 
-									allowtransparency="true">
-							</iframe> 
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- Modal parte 2-->
-		<script type="text/javascript">
-			function previewProducto(idProducto) {
-				var src = '<?=$context?>/site/admin/mvc/view/producto/viewProducto.php?idproducto=' + idProducto;
-				var height = $(this).attr('data-height') || '450px';
-				var width = $(this).attr('data-width') || '100%';
-				
-				$("#targetiframe").attr({
-					'src': src,
-					'height': height,
-					'width': width
-				});
-
-			}
-		</script>
+		<?php include "../../site/template/modal.php";?>
 
 		<script src="https://cdn.shopify.com/s/files/1/0908/7252/t/2/assets/cs.global.js?14058599523483859647" type="text/javascript"></script> 
 		<script type="text/javascript">
