42365331afa53c413d482e5ad3bcd0dbd7e6e2ea
Jorge <neiro70@gmail.com>
2018-04-17 10:39:49 -0500

    cambio de imagen madres

--- site/index.php
+++ site/index.php
@@ -92,7 +92,7 @@
 				<div class="home-slider-wrapper clearfix">
 					<div class="camera_wrap" id="home-slider">
 						
-						<div data-src="<?php echo $context ?>/site/img/collection/índice.png">
+						<div data-src="<?php echo $context ?>/site/img/collection/madre.jpg">
 							<div class="camera_caption camera_title_1 fadeIn moveFromLeft">	
 							
 									<p style="color: #010101;" class='chopsFont' > Bienvenido </p>
